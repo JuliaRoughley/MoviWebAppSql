0e9c9a233bffeeeedc6c9ca4d5385f3c8acbca77
JuliaRoughley <roughleyjulia@gmail.com>
2023-08-29 14:02:07 +0100

    SQLLite tests running as expected - but SQLLite calls failed

--- DataManager/SQLlite_data_manager.py
+++ DataManager/SQLlite_data_manager.py
@@ -1,15 +1,16 @@
-from flask_sqlalchemy import SQLAlchemy
+import sqlite3
 from DataManager.data_manager_interface import DataManagerInterface
 
 class SQLiteDataManager(DataManagerInterface):
     def __init__(self, db_file_name):
-        self.db = SQLAlchemy(db_file_name)
-
+        self.db_connection = sqlite3.connect(db_file_name)
 
     def get_all_users(self):
         """To get a list of all the users who have an account in the moviweb app"""
-        sql_query = "SELECT name FROM users"
-        result = self.db.engine.execute(sql_query)
+        sql_query = "SELECT name FROM Users"
+        cursor = self.db_connection.cursor()
+        cursor.execute(sql_query)
+        result = cursor.fetchall()
 
         user_names = [row[0] for row in result]
         return user_names
@@ -28,3 +29,10 @@ class SQLiteDataManager(DataManagerInterface):
     def add_new_movie(self):
         """To add a new movie to a users list of favourite movies in the moviweb app"""
         pass
+
+
+
+
+""" test_class = SQLiteDataManager("movie_web_app.db")
+print(test_class.get_all_users())
+test_class.db_connection.close() """
--- tests/test_sqldatamanager.py
+++ tests/test_sqldatamanager.py
@@ -1,8 +1,11 @@
 import pytest
 from DataManager.SQLlite_data_manager import SQLiteDataManager
 
+@pytest.fixture
+def db_manager():
+    return SQLiteDataManager(":memory:")
+
 def test_get_all_users():
-    db_manager = SQLiteDataManager("test_movie_web_app.db")
     user_names = db_manager.get_all_users()
-    print(user_names)
-    assert True
+    expected_user_names = ["Alice", "Bob"]  # Replace with your expected names
+    assert user_names == expected_user_names
